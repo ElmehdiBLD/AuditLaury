7551b218a3188610aaaa999c17dc6f8da549af4b
AlexandreChauvin <alexandre.chauvin96@yahoo.fr>
2019-07-09 15:27:11 +0200

    :construction: page connection :construction:

---
+++ AuditLaury/login.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    MySqlConnection con = new MySqlConnection(@"Data Source=localhost;port=3306;Initial Catalog=auditlaury;User Id=root;Password=''");
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        MySqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "select * from users where login ='" + t1.Text + "'and password ='" + t2.Text + "'";
+        cmd.ExecuteNonQuery();
+        DataTable dt = new DataTable();
+        MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+        da.Fill(dt);
+        foreach (DataRow dr in dt.Rows)
+        {
+            Session["login"] = dr["login"].ToString();
+            Response.Redirect("general.aspx");
+        }
+
+        label1.Text = "Mot de passe ou login incorrect";
+
+        con.Close();
+    }
+}
